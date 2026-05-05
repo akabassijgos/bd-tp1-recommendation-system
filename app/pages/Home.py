2c9079747fa677814b33420eb53b75b5519ed6be
AKABASSI Jesugnon Godwin Olaré Wadjou Samuel <akabassijgos@gmail.com>
2026-05-05 18:25:06 +0100

    fix(ui): position the load more button to the left

--- app/pages/Home.py
+++ app/pages/Home.py
@@ -46,17 +46,6 @@ st.markdown("""
     border-radius: 10px;
 }
 
-.center-btn {
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-}
-
-.center-btn button {
-    width: 200px;
-    border-radius: 10px;
-}
-
 </style>
 """, unsafe_allow_html=True)
 
@@ -182,19 +171,11 @@ if search_active:
 
     render_grid(st.session_state.results)
 
-    st.markdown("<br><br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
 
-    col1, col2, col3 = st.columns([1, 2, 1])
-
-    with col2:
-        st.markdown('<div class="center-btn">', unsafe_allow_html=True)
-
-        if st.button("Charger plus"):
-            st.session_state.page += 1
-            st.rerun()
-
-        st.markdown('</div>', unsafe_allow_html=True)
-
+    if st.button("Charger plus"):
+        st.session_state.page += 1
+        st.rerun()
 
 # =====================================================
 # MODE 2 : HOME MODE (RECO + POPULAR)
