1aeabe85ce3337baec7ef0e3456fcd5b565b938c
AKABASSI Jesugnon Godwin Olaré Wadjou Samuel <akabassijgos@gmail.com>
2026-05-05 14:37:51 +0100

    feat(ui): add movie search and genre filtering to home page

--- app/pages/Home.py
+++ app/pages/Home.py
@@ -13,6 +13,29 @@ if not user:
     st.warning("Veuillez vous connecter")
     st.stop()
 
+
+# Recherche
+search_query = st.text_input("Rechercher un film")
+
+
+# Filtre par genre
+genres_list = extract_genres(movies)
+selected_genre = st.selectbox("Filtrer par genre", ["Tous"] + genres_list)
+
+
+# Appliquer Recherche et Filtre
+filtered_movies = movies.copy()
+
+if search_query:
+    filtered_movies = filtered_movies[
+        filtered_movies["title"].str.contains(search_query, case=False)
+    ]
+
+if selected_genre != "Tous":
+    filtered_movies = filtered_movies[
+        filtered_movies["genres"].str.contains(selected_genre)
+    ]
+
 # Toggle affichage
 view_mode = st.radio(
     "Mode d'affichage",
@@ -26,7 +49,7 @@ mode = get_recommendation_mode(user["id"], ratings)
 if mode == "popular":
     st.warning("Recommandations générales (popularité)")
     recs = get_popular_movies(20)
-    recs = recs.merge(movies, left_on="id", right_on="id")
+    recs = recs.merge(filtered_movies, left_on="id", right_on="id")
 
 else:
     st.success("Recommandations personnalisées")
@@ -34,7 +57,7 @@ else:
     sim = compute_item_similarity(matrix)
 
     rec_ids = recommend_items(user["id"], ratings, matrix, user_map, movie_map, sim, 20)
-    recs = movies[movies["id"].isin(rec_ids)]
+    recs = filtered_movies[filtered_movies["id"].isin(rec_ids)]
 
 # Affichage
 if view_mode == "Scroll":
