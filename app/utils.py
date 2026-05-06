4ed7015544fb79dad2b185d9021535fe5ca8d0fe
AKABASSI Jesugnon Godwin Olaré Wadjou Samuel <akabassijgos@gmail.com>
2026-05-06 13:15:06 +0100

    chore: delete unused files

--- app/utils.py
+++
@@ -1,9 +0,0 @@
-
-def extract_genres(movies_df):
-    genres_set = set()
-
-    for genres in movies_df["genres"]:
-        for g in genres.split("|"):
-            genres_set.add(g)
-
-    return sorted(genres_set)
